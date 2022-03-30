ac7abd9a4c7684186444cc3abcb7e7bb52e10e8f
leon3131-ux <56247087+Leon3131-ux@users.noreply.github.com>
2022-03-30 19:48:43 +0200

    added dockerfile for frontend

--- mbedDataCollectorBackend/src/main/java/com/woods/mbedstuff/config/WebSocketConfig.java
+++ mbedDataCollectorBackend/src/main/java/com/woods/mbedstuff/config/WebSocketConfig.java
@@ -11,7 +11,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket").setAllowedOrigins("http://localhost:4200");
+        registry.addEndpoint("/websocket").setAllowedOrigins("/**");
     }
 
 }
