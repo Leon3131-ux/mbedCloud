fa53496b53b0c06466ec35f8ba33aa025a82855e
leon3131-ux <knud.del@hotmail.com>
2022-04-01 10:36:58 +0200

    little update

--- mbedDataCollectorFrondend/src/app.js
+++ mbedDataCollectorFrondend/src/app.js
@@ -1,9 +1,15 @@
 const socket = new SockJS("http://164.92.173.232:23552/websocket");
 const stompClient = Stomp.over(socket);
 
-stompClient.connect({}, () => {
-    console.log("successfully connected");
-    stompClient.subscribe("/topic/temp", function (temp){
-        console.log("temperature: " + temp);
-    });
-})
+$( document ).ready(function() {
+    stompClient.connect({}, () => {
+        console.log("successfully connected");
+        stompClient.subscribe("/topic/temp", function (temp){
+            console.log("temperature: " + temp);
+        });
+    })
+});
+
+$(window).on('beforeunload', function(){
+    socket.close();
+});
