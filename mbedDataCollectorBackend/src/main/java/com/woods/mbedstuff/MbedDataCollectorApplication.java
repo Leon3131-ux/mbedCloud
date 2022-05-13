b7e6be712f6b3dbf82d3115bdd5ea14e7e2f0bab
leon3131-ux <knud.del@hotmail.com>
2022-05-13 13:46:59 +0200

    bugfix

--- mbedDataCollectorBackend/src/main/java/com/woods/mbedstuff/MbedDataCollectorApplication.java
+++ mbedDataCollectorBackend/src/main/java/com/woods/mbedstuff/MbedDataCollectorApplication.java
@@ -2,8 +2,10 @@ package com.woods.mbedstuff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class MbedDataCollectorApplication {
 
     public static void main(String[] args) {
