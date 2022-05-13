c11cc8ce9ec039ffed51d3a04fcdad69df7c8f76
leon3131-ux <knud.del@hotmail.com>
2022-05-13 14:17:38 +0200

    added display text

--- mbedDataCollectorBackend/src/main/java/com/woods/mbedstuff/controller/DataController.java
+++ mbedDataCollectorBackend/src/main/java/com/woods/mbedstuff/controller/DataController.java
@@ -1,41 +1,22 @@
 package com.woods.mbedstuff.controller;
 
-import com.woods.mbedstuff.dto.SensorDataDto;
+import com.woods.mbedstuff.dto.DisplayTextDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class DataController {
 
-    private final SimpMessagingTemplate simpMessagingTemplate;
+    private final JmsTemplate jmsTemplate;
 
-    @RequestMapping(value = "/api/sensor/data", method = RequestMethod.POST)
-    public ResponseEntity<?> getSensorData(@RequestBody List<SensorDataDto> sensorDataDtos){
-        for(SensorDataDto dto : sensorDataDtos){
-            switch (dto.getType()){
-                case TEMP:
-                    simpMessagingTemplate.convertAndSend("/topic/temp", dto.getValue());
-                    break;
-                case HUM:
-                    simpMessagingTemplate.convertAndSend("/topic/humidity", dto.getValue());
-                    break;
-                case BEATC:
-                    simpMessagingTemplate.convertAndSend("/topic/gyro", dto.getValue());
-                    break;
-                case BTNC:
-                    simpMessagingTemplate.convertAndSend("/topic/btnc", dto.getValue());
-                    break;
-                case RFID:
-                    simpMessagingTemplate.convertAndSend("/topic/rfid", dto.getValue());
-            }
-        }
+    @RequestMapping(value = "/api/display", method = RequestMethod.POST)
+    public ResponseEntity<?> getSensorData(@RequestBody DisplayTextDto dto){
+        jmsTemplate.convertAndSend("iotkit.display", dto.getDisplayText());
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
---
+++ mbedDataCollectorFrondend/src/display/display.js
@@ -0,0 +1,22 @@
+const socket = new SockJS("http://164.92.173.232:23552/websocket");
+const stompClient = Stomp.over(socket);
+
+let display = null;
+
+$( document ).ready(function() {
+    $("#navbar").load("../shared/navbar.html", function (){
+        $("#displayLink").attr("aria-current", "true").addClass("active");
+    });
+});
+
+function submitText() {
+    $.ajax({
+        type: 'POST',
+        url: 'http://164.92.173.232:23552/api/display',
+        data: JSON.stringify({"displayText": $("#displayText").innerText}),
+        success: function (){
+            console.log('Display text submitted successfully');
+        },
+        dataType: 'text/json'
+    })
+}
